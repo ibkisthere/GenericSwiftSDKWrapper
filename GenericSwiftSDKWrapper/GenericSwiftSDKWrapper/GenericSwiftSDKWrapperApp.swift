ff612fd40004c0ba033a4e195b6af663527db234
ibkisthere <ibukunoluwaakintobi@gmail.com>
2024-06-07 01:36:05 +0100

    chore:initilaized new project

---
+++ GenericSwiftSDKWrapper/GenericSwiftSDKWrapper/GenericSwiftSDKWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericSwiftSDKWrapperApp.swift
+//  GenericSwiftSDKWrapper
+//
+//  Created by Ibukunoluwa Akintobi on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenericSwiftSDKWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
